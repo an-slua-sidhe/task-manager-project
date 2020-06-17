1371c5631a341c82306a67dc9b91fff9ab0e0093
an-slua-sidhe <polocoinn@yahoo.ie>
2020-06-17 15:27:31 +0100

    Retry with CI lesson code

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://polocoinn:Ansluasidhe2@myfirstcluster-c89ie.mongodb.net/task_manager?retryWrites=true&w=majority')
 
-
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_tasks')
@@ -20,5 +18,5 @@ def get_tasks():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=os.environ.get('PORT'),
-        debug=True)
+            port=os.environ.get('PORT'),
+            debug=True)
